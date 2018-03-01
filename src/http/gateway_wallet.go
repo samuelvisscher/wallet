1005b19b76a0b8bf6d980e470f2062bb1c75b9fc
Evan Lin <evan.linjin@gmail.com>
2018-03-01 16:21:05 +0800

    added comments of wallet api implementations

--- src/http/gateway_wallet.go
+++ src/http/gateway_wallet.go
@@ -6,5 +6,58 @@ import (
 )
 
 func walletGateway(mux *http.ServeMux, g *wallet.Manager) error {
+
+	Handle(mux, "/api/wallets/refresh",
+		"GET", refreshWallets(g))
+
+	Handle(mux, "/api/wallets/list",
+		"GET", listWallets(g))
+
+	Handle(mux, "/api/wallets/new",
+		"POST", newWallet(g))
+
 	return nil
 }
+
+func refreshWallets(g *wallet.Manager) HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request, p *Path) error {
+		// TODO: implement.
+		// CALLS: 'g.Refresh()'.
+		// RESPONSE:
+		// - 'Content-Type': 'application/json'.
+		// - true & 200   : on success.
+		// - string & 500 : of the error on failure.
+		return nil
+	}
+}
+
+type WalletsReply struct {
+	Wallets []wallet.Stat `json:"wallets"`
+}
+
+func listWallets(g *wallet.Manager) HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request, p *Path) error {
+		// TODO: implement.
+		// CALLS: 'g.ListWallets()'.
+		// RESPONSE:
+		// - 'Content-Type': 'application/json'.
+		// - json representation of 'WalletsReply'.
+		return nil
+	}
+}
+
+func newWallet(g *wallet.Manager) HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request, p *Path) error {
+		// TODO: implement.
+		// CALLS: 'g.NewWallet()'.
+		// REQUEST:
+		// - 'Content-Type': 'application/x-www-form-urlencoded'.
+		// - Check actual function for all key-values.
+		// RESPONSE:
+		// - 'Content-Type': 'application/json'.
+		// - true & 200   : on success.
+		// - string & http status code : of the error on failure.
+		return nil
+	}
+}
+
