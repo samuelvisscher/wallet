e47b4c86a41cd844406abce30dc54cb84f7a9f2e
Randy1Burrell <Randy1burrell@yahoo.com>
2018-02-28 21:37:43 -0500

    Testing for Verify to throw error if transaction hash has been changed

--- src/iko/transaction_test.go
+++ src/iko/transaction_test.go
@@ -1,7 +1,7 @@
 package iko
 
 import (
-/*	"errors"*/
+	/*	"errors"*/
 	"fmt"
 	"github.com/skycoin/skycoin/src/cipher"
 	"github.com/stretchr/testify/require"
@@ -21,23 +21,47 @@ func runTransactionVerifyTest(t *testing.T, stateDB StateDB) {
 	})
 	anAddress := cipher.AddressFromSecKey(sk)
 
-	t.Run("TransactionCreated_NoTransactionAvailable", func(t *testing.T) {
+	t.Run("TransactionCreated_InvalidPrevTransaction", func(t *testing.T) {
 		// Add a kitty
 		txHash := TxHash(cipher.SumSHA256([]byte{3, 4, 5, 6}))
 		kID := KittyID(3)
 
 		err := stateDB.AddKitty(txHash, kID, anAddress)
+
+		// If there's an error creating kitty, then deviate testing transaction -- no kitty means no transaction
 		if err == nil {
 			prev := NewGenTx(nil, kID, sk)
-			// Since there is no previous transactions then an error should be thrown
-			require.EqualError(t, prev.Verify(prev), "invalid prev hash")
+			// Since there is no previous transactions then an error should be thrown that previous hash is invalid
+			require.Errorf(t, prev.Verify(prev), "There is no previous tansaction to verify")
 		} else {
 			fmt.Println("StateDB error, failed to create Kitty")
 		}
 	})
 
-	fmt.Println(sk)
+	t.Run("TransactionCreated_InvalidPrevHash", func(t *testing.T) {
+		kID := KittyID(3)
+
+		// New secret key for toAddress
+		sk2 := cipher.SecKey([32]byte{
+			3, 4, 5, 6,
+			3, 4, 5, 6,
+			3, 4, 5, 6,
+			3, 4, 5, 6,
+			5, 4, 3, 6,
+			3, 4, 5, 6,
+			4, 4, 5, 6,
+			3, 4, 5, 6,
+		})
+		toAddress := cipher.AddressFromSecKey(sk2)
+		prev := NewGenTx(nil, kID, sk)
+		nextTrans := NewTransferTx(prev, kID, toAddress, sk)
+
+		fmt.Println(nextTrans.Seq)
+		nextTrans.Prev = TxHash(cipher.SumSHA256([]byte{3, 4, 5, 6}))
+		require.Errorf(t, nextTrans.Verify(prev), "Previous hash was changed!!")
+	})
 }
+
 func TestTransaction_Verify(t *testing.T) {
 	stateDB := NewMemoryState()
 	runTransactionVerifyTest(t, stateDB)
